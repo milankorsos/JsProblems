fbde535db600a6a0a0d74151d92897e5d8748847
Milan Korsos <milan.korsos@hgdata.com>
2017-03-16 10:57:55 -0700

    Coins: code cleanup

--- misc/coin.js
+++ misc/coin.js
@@ -25,42 +25,64 @@
 
 */
 
-const cashier = function(amount, denominations, results = [], tempResults = []) {
-  const count = tempResults.reduce((acc, value) => acc + value, 0)
+const getSumOfArray = function(arr) {
+  return arr.reduce((acc, value) => {
+    return acc + value;
+  }, 0);
+}
 
-  for (let i = 0; i < denominations.length; i++) {
-    const denomination = denominations[i];
+// console.log('sum', getSumOfArray([1, 2, 3]) === 6);
+// console.log('sum', getSumOfArray([]) === 0);
+// console.log('sum', getSumOfArray([0]) === 0);
 
-    const newTempResults = Array.from(tempResults);
-    newTempResults.push(denomination)
-
-    // if equals
-    if (count + denomination === amount) {
-      newTempResults.sort((a, b) => a > b ? 1 : -1);
-      // check if this result is already in the results
-      const dupe = results.find(arr => {
-        if (arr.length !== newTempResults.length) {
+const isArrayHasArrayItem = function(arr, item) {
+  const found = arr.find(arrItem => {
+    if (arrItem.length !== item.length) {
+      return false;
+    } else {
+      arrItem.sort();
+      item.sort();
+      for (let i = 0; i < arrItem.length; i++) {
+        if (arrItem[i] !== item[i]) {
           return false;
-        } else {
-          for (let k = 0; k < arr.length; k++) {
-            if (arr[k] !== newTempResults[k]) {
-              return false;
-            }
-          }
-          return true;
         }
-      })
-      if (!dupe) {
-        results.push(newTempResults);
       }
-      return;
+      return true;
+    }
+  });
+  return !!found;
+}
+
+// console.log('array', isArrayHasArrayItem([], [1, 2]) === false);
+// console.log('array', isArrayHasArrayItem([[1, 2], [2, 3]], [1, 2]) === true);
+// console.log('array', isArrayHasArrayItem([[2, 1], [2, 3]], [1, 2]) === true);
+// console.log('array', isArrayHasArrayItem([[1, 3], [2, 3]], [1, 2]) === false);
+// console.log('array', isArrayHasArrayItem([[1, 3], [2, 3, 4]], [1, 2]) === false);
+
+
+const cashier = function(amount, denominations, results = [], tempResult = []) {
+  const sum = getSumOfArray(tempResult);
+
+  denominations.forEach(denomination => {
+    const result = Array.from(tempResult);
+    result.push(denomination);
+
+    // Check if success
+    if (sum + denomination === amount) {
+      if (!isArrayHasArrayItem(results, result)) {
+        results.push(result);
+      }
+      return true;
     }
 
-    // otherwise keep looking
-    cashier(amount, denominations, results, newTempResults);
-  }
+    // Check if overshooting
+    if (sum + denomination > amount) {
+      return false;
+    }
 
-  console.log('results', results)
+    // Otherwise, keep looking recursively
+    cashier(amount, denominations, results, result);
+  });
 
   return results.length;
 }
--- misc/coin.test.js
+++ misc/coin.test.js
@@ -3,3 +3,7 @@ import cashier from './coin';
 test('cashier', () => {
   expect(cashier(4, [1, 2, 3])).toEqual(4);
 })
+
+test('cashier', () => {
+  expect(cashier(4, [5, 10])).toEqual(0);
+})
