881d66d4604937bbb303667391cdac8fc44187e7
Milan Korsos <milan.korsos@hgdata.com>
2017-03-15 17:59:55 -0700

    Improve algo to run in I(n*log(n)) instead of O(n^2)

--- misc/mergeRanges.js
+++ misc/mergeRanges.js
@@ -11,38 +11,49 @@
     {startTime: 2, endTime: 3} // meeting from 10:00 – 10:30 am
     {startTime: 6, endTime: 9} // meeting from 12:00 – 1:30 pm
 
-  Write a function mergeRanges() that takes an array of meeting time ranges and returns an array of
-  condensed ranges.
+  Write a function mergeRange() that takes an array of meeting time mergedMeetings and returns an array of
+  condensed mergedMeetings.
 
   Do not assume the meetings are in order. The meeting times are coming from multiple teams.
 
   Write a solution that's efficient even when we can't put a nice upper bound on the numbers
-  representing our time ranges. Here we've simplified our times down to the number of 30-minute
+  representing our time mergedMeetings. Here we've simplified our times down to the number of 30-minute
   slots past 9:00 am. But we want the function to work even for very large numbers, like Unix
   timestamps. In any case, the spirit of the challenge is to merge meetings where startTime and
   endTime don't have an upper bound.
 
-  https://www.interviewcake.com/question/javascript/merging-ranges
+  https://www.interviewcake.com/question/javascript/merging-mergedMeetings
 
 */
 
-const mergeRanges = function(meetings) {
-  const ranges = [];
+const mergeRange = function(meetings) {
+  const mergedMeetings = [];
 
+  // Sort meetings first O(n*log(n))
+  meetings.sort((first, second) => {
+    // Sort by startTime first, of equal, then sort by endTime
+    if (first.startTime === second.startTime) {
+      return first.endTime < second.endTime ? -1 : 1;
+    }
+    return first.startTime < second.startTime ? -1 :1;
+  });
+
+  // Iterate through meetings. Either merge to the last range, or push it as new. O(n)
   meetings.forEach(meeting => {
-    const overlappingRange = ranges.find(range => {
-      return !(meeting.endTime < range.startTime || meeting.startTime > range.endTime);
-    });
+    const mergedMeeting = mergedMeetings[mergedMeetings.length - 1];
 
-    if (!overlappingRange) {
-      ranges.push(meeting);
+    const isOverlapping = mergedMeeting &&
+      !(meeting.endTime < mergedMeeting.startTime || meeting.startTime > mergedMeeting.endTime);
+
+    if (isOverlapping) {
+      mergedMeeting.startTime = Math.min(mergedMeeting.startTime, meeting.startTime);
+      mergedMeeting.endTime = Math.max(mergedMeeting.endTime, meeting.endTime);
     } else {
-      overlappingRange.startTime = Math.min(overlappingRange.startTime, meeting.startTime);
-      overlappingRange.endTime = Math.max(overlappingRange.endTime, meeting.endTime);
+      mergedMeetings.push(meeting);
     }
   });
 
-  return ranges;
+  return mergedMeetings;
 }
 
-export default mergeRanges;
+export default mergeRange;
